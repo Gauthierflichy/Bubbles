83a4e81f975c00d192545ca7069e0e79ff3cbf78
Gauthier FLICHY <gauthier.flichy@monpotager.com>
2016-10-19 16:10:01 +0200

    Added array

--- client/main.js
+++ client/main.js
@@ -3,4 +3,17 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-if (Meteor.isClient) {    Template.game.game = function(){       /*all your game code here*/   }}
+if (Meteor.isClient) {
+    Template.game.game = function(){
+        var niveau = [
+
+            [0,1,0,0,0,1,0,1,0,1,1,0],
+            [0,1,0,0,0,1,0,1,0,1,1,0],
+            [0,1,0,0,0,1,0,1,0,1,1,0],
+            [0,1,0,0,0,1,0,1,0,1,1,0],
+            [0,1,0,0,0,1,0,1,0,1,1,0],
+            [0,1,0,0,0,1,0,1,0,1,1,0]
+        ]
+        
+    }
+}
