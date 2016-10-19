72944c244e0a3f6e09f9f2048215f0d9864d526a
Gauthier FLICHY <gauthier.flichy@monpotager.com>
2016-10-19 17:20:16 +0200

    Added random color

--- client/main.js
+++ client/main.js
@@ -24,18 +24,24 @@ if (Meteor.isClient) {
         }
         function create(){
 
+            game.stage.backgroundColor = "#4488AA";
             var graphics = game.add.graphics(0,0);
             var diameter = 50;
+
+            var colors = ['000080', '009926', '880000', 'dd1144', 'E9DC51'];
+ 
             for(var i=0;i<6;i++){
                 console.log("ok");
                 for(var j=0;j<12;j++){
                     if(niveau[i][j]==1){
                         var randDiam = parseInt((diameter) * (Math.random(1.5,3)));
-                        console.log(randDiam);
-                        graphics.beginFill(0xFF00000, 0.4);
+                        var circleColor = '0x'+ colors[parseInt(Math.random()*5)];
+                        console.log(circleColor);
+
+                        graphics.beginFill(circleColor, 0.4);
                         graphics.drawCircle(50*(j+1),50*(i+1),diameter);
                         graphics.endFill();
-                        graphics.beginFill(0xFF00000, 1);
+                        graphics.beginFill(circleColor, 1);
                         graphics.drawCircle(50*(j+1),50*(i+1),randDiam);
                         graphics.endFill();
 
